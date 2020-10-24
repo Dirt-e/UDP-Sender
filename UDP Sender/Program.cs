9a4daf5f77f3539871fb42144fca1fd78a3144e4
Dirt-e <50072319+Dirt-e@users.noreply.github.com>
2020-10-24 22:56:07 +0200

    Initial commit

---
+++ UDP Sender/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP_Sender
+{
+    class Program
+    {
+        static void Main()
+        {
+            string Message;
+            int FromPort = 9999;
+            string ToIP = "127.0.0.1";
+            int ToPort = 10000;
+            while (true)
+            {
+                Message = Console.ReadLine();
+                SendUdp(FromPort, ToIP, ToPort, Message) ;
+            }
+        }
+
+        static void SendUdp(int srcPort, string dstIp, int dstPort, string msg)
+        {   
+            byte[] bytes = Encoding.ASCII.GetBytes(msg);
+
+            using (UdpClient c = new UdpClient(srcPort))
+                c.Send(bytes, bytes.Length, dstIp, dstPort);
+        }
+    }
+}
